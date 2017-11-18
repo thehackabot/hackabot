927b764e6e4ec52ba531cab46348880885f56871
Lennart Espe <lnsp@users.noreply.github.com>
2017-11-18 19:52:28 +0100

    Add emotions to match skill

--- src/main/java/io/ahababo/bot/skills/drinking/MatchSkill.java
+++ src/main/java/io/ahababo/bot/skills/drinking/MatchSkill.java
@@ -3,6 +3,7 @@ package io.ahababo.bot.skills.drinking;
 import io.ahababo.bot.Bot;
 import io.ahababo.bot.User;
 import io.ahababo.bot.skills.StatefulSkill;
+import io.ahababo.bot.skills.util.EmotionDetector;
 import org.telegram.telegrambots.api.methods.send.SendMessage;
 import org.telegram.telegrambots.api.methods.send.SendPhoto;
 import org.telegram.telegrambots.api.objects.Message;
@@ -10,52 +11,76 @@ import org.telegram.telegrambots.api.objects.Message;
 import java.util.ArrayList;
 
 public class MatchSkill extends StatefulSkill {
+    private final String MATCH_ALREADY_LOOKING = "Sorry, but you are already looking for a match.";
+    private final String MATCH_TAKE_PHOTO = "Please take a photo of yourself.";
+    private final String MATCH_TOO_MANY_PERSON = "There seem to be too many persons on the picture. Please remove any unnecessary humans.";
+    private final String MATCH_NO_PERSON = "You seem to be invisble to me. Please try again.";
+    private final String MATCH_ERROR = "I was not able to evaluate you. Please try again later.";
+    private final String MATCH_DESCRIBE = "Please describe yourself.";
+    private final String MATCH_IGNORE = "Well, if you don't want to.";
+    private final String MATCH_FOUND = "Your search was successful. You are %.2f%% similar to your match. He/she describes himself/herself as '%s'.";
+    private final String MATCH_IN_QUEUE = "You are a right proper lad. We will find a match for you.";
+
     private String description, image;
+    private EmotionDetector.Result emotions;
 
     private SendMessage handleWelcomeState(Message incoming) {
         if (CommonContext.getInstance().exists(getUser())) {
             increaseState(); increaseState(); increaseState();
-            return new SendMessage().setChatId(incoming.getChatId()).setText("Sorry, but you are already looking for partner.");
+            return new SendMessage().setChatId(incoming.getChatId()).setText(MATCH_ALREADY_LOOKING);
         } else {
             increaseState();
-            return new SendMessage().setChatId(incoming.getChatId()).setText("Please take a photo of yourself.");
+            return new SendMessage().setChatId(incoming.getChatId()).setText(MATCH_TAKE_PHOTO);
         }
     }
 
     private SendMessage handleSelfieState(Message incoming) {
         if (incoming.getPhoto() != null) {
-            increaseState();
             image = incoming.getPhoto().get(incoming.getPhoto().size()-1).getFileId();
-            System.out.println(incoming.getPhoto().get(incoming.getPhoto().size()-1));
-            return new SendMessage().setChatId(incoming.getChatId()).setText("Please describe yourself.");
+            try {
+                EmotionDetector.Result[] output = EmotionDetector.detect(getContext(), image);
+                if (output.length > 1) {
+                    return new SendMessage().setChatId(incoming.getChatId()).setText(MATCH_TOO_MANY_PERSON);
+                } else if (output.length < 1) {
+                    return new SendMessage().setChatId(incoming.getChatId()).setText(MATCH_NO_PERSON);
+                }
+                emotions = output[0];
+            } catch (Exception e) {
+                increaseState();
+                increaseState();
+                return new SendMessage().setChatId(incoming.getChatId()).setText(MATCH_ERROR);
+            }
+            increaseState();
+            return new SendMessage().setChatId(incoming.getChatId()).setText(MATCH_DESCRIBE);
         } else {
             increaseState();
             increaseState();
-            return new SendMessage().setChatId(incoming.getChatId()).setText("Well, if you don't want to.");
+            return new SendMessage().setChatId(incoming.getChatId()).setText(MATCH_IGNORE);
         }
     }
 
     private SendMessage handleDescribeState(Message incoming) {
         increaseState();
         description = incoming.getText();
-        CommonContext.MatchEntry entry = CommonContext.getInstance().lookForMatch(getUser());
+        CommonContext.MatchEntry entry = CommonContext.getInstance().lookForMatch(getUser(), emotions);
         if (entry != null) {
+            double similarity = (entry.emotions.similarTo(emotions) / 0.5 + 0.5) * 100.0;
             getContext().publish(new SendMessage()
                     .setChatId(entry.match.getChatId())
-                    .setText("Your search was successful! Your match describes itself as '" + description + "'."));
+                    .setText(String.format(MATCH_FOUND, similarity, description)));
             getContext().publish(new SendPhoto()
                     .setChatId(entry.match.getChatId())
                     .setPhoto(image));
             CommonContext.getInstance().accept(entry);
             getContext().publish(new SendMessage()
                     .setChatId(incoming.getChatId())
-                    .setText("I may have found a match. It describes itself as '" + entry.description + "'."));
+                    .setText(String.format(MATCH_FOUND, similarity, entry.description)));
             getContext().publish(new SendPhoto()
                     .setChatId(incoming.getChatId())
                     .setPhoto(entry.image));
-            } else {
-            CommonContext.getInstance().enqueue(getUser(), description, image);
-            return new SendMessage().setChatId(incoming.getChatId()).setText("Hackabot strong, Hackabot looking!");
+        } else {
+            CommonContext.getInstance().enqueue(getUser(), description, image, emotions);
+            return new SendMessage().setChatId(incoming.getChatId()).setText(MATCH_IN_QUEUE);
         }
         return null;
     }
@@ -83,11 +108,13 @@ public class MatchSkill extends StatefulSkill {
         private class MatchEntry {
             final User match;
             final String description, image;
+            final EmotionDetector.Result emotions;
 
-            public MatchEntry(User match, String description, String image) {
+            public MatchEntry(User match, String description, String image, EmotionDetector.Result emotions) {
                 this.match = match;
                 this.description = description;
                 this.image = image;
+                this.emotions = emotions;
             }
 
             @Override
@@ -102,18 +129,26 @@ public class MatchSkill extends StatefulSkill {
         private CommonContext() {
             openMatches = new ArrayList<>();
         }
-        public void enqueue(User user, String description, String image) {
-            openMatches.add(new MatchEntry(user, description, image));
+        public void enqueue(User user, String description, String image, EmotionDetector.Result emotions) {
+            openMatches.add(new MatchEntry(user, description, image, emotions));
 
         }
         public boolean exists(User match) {
-            return lookForMatch(match) == null && openMatches.size() > 0;
-        }
-        public MatchEntry lookForMatch(User partner) {
             for (MatchEntry entry : openMatches) {
-                if (!entry.match.equals(partner)) return entry;
+                if (entry.match.equals(match)) return true;
             }
-            return null;
+            return false;
+        }
+        public MatchEntry lookForMatch(User partner, EmotionDetector.Result emotions) {
+            MatchEntry max = null;
+            double maxScore = -1;
+            for (MatchEntry entry : openMatches) {
+                if (!entry.match.equals(partner) && entry.emotions.similarTo(emotions) > maxScore) {
+                    maxScore = entry.emotions.similarTo(emotions);
+                    max = entry;
+                }
+            }
+            return max;
         }
         public void accept(MatchEntry match) {
             openMatches.remove(match);
