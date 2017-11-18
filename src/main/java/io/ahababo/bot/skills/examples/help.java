838ac3925c3e7f31612027ac2cb072391029c3a8
lifelikemaik <maikluu@googlemail.com>
2017-11-18 06:04:59 +0100

    add uncomplete HangmanSkill v0.5 add help

--- src/main/java/io/ahababo/bot/skills/examples/help.java
+++ src/main/java/io/ahababo/bot/skills/examples/help.java
@@ -1,27 +1,18 @@
 package io.ahababo.bot.skills.examples;
 
-import io.ahababo.bot.User;
+
 import io.ahababo.bot.skills.BasicSkill;
 import org.telegram.telegrambots.api.methods.send.SendMessage;
 import org.telegram.telegrambots.api.objects.Message;
 
 public class help extends BasicSkill{
 
-  public help(){ super();}
-
-  public void init(User user){
-    super.init(user);
-  }
-
-  String message = "hello";
 
   @Override
   public SendMessage handle(Message incoming) {
-
-
-    return new SendMessage().setChatId(incoming.getChatId()).setText(message);
+    return new SendMessage().setChatId(incoming.getChatId()).setText("hello\nguess\ngood bot\nbeer\nRockPaperScissor");
   }
 
-  public boolean isFinished(){return true;}
+
 
 }
