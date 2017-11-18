4c30d547afb6d6014e42edc140abc639f8c9ddff
Patrick <Patrick.Bettermann@tum.de>
2017-11-18 20:04:27 +0100

    Power Point Karaoke shows slide

--- src/main/java/io/ahababo/bot/skills/party/PowerPointKaraokeSkill.java
+++ src/main/java/io/ahababo/bot/skills/party/PowerPointKaraokeSkill.java
@@ -3,37 +3,29 @@ package io.ahababo.bot.skills.party;
 import io.ahababo.bot.Bot;
 import io.ahababo.bot.User;
 import io.ahababo.bot.skills.BasicSkill;
-import io.ahababo.bot.skills.StatefulSkill;
+import org.telegram.telegrambots.api.methods.send.SendDocument;
 import org.telegram.telegrambots.api.methods.send.SendMessage;
 import org.telegram.telegrambots.api.methods.send.SendPhoto;
 import org.telegram.telegrambots.api.objects.Message;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
 
 import java.util.Random;
 
-public class PowerPointKaraokeSkill extends StatefulSkill{
+public class PowerPointKaraokeSkill extends BasicSkill{
+    public SendMessage handle(Message message) {
+        String botmsg = "You started a round of PowerPointKaraoke. Have fun! \n";
+        botmsg += "Selected random player to present a slide\n";
 
+        int playerIndex = new Random().nextInt() % getContext().getUserPhotosLength();
+        User player = getContext().getActiveUsers().get(playerIndex);
+        getContext().publish(new SendPhoto().setChatId(message.getChatId()).setPhoto(getContext().getUserPhoto(player)));
 
-    public void init(Bot context, User user, int maxStates){
-        super.init(context, user);
-            maxStates = maxStates;
-            maxStates = 0;
-    }
+        try{
+            getContext().sendPhoto(new SendPhoto().setChatId(message.getChatId()).setPhoto("ahababo/PowerpointKaraoke/Folie1.JPG"));
+        }catch(TelegramApiException e){
 
-    public SendMessage transition(Message message, int state) {
-        switch (state){
-            case 0:
-                String botmsg = "You started a round of PowerPointKaraoke. Have fun! \n";
-                botmsg += "Selected random player to present a slide\n";
-                int playerIndex = new Random().nextInt() % getContext().getUserPhotosLength();
-                User player = getContext().getActiveUsers().get(playerIndex);
-                getContext().publish(new SendPhoto().setChatId(message.getChatId()).setPhoto(getContext().getUserPhoto(player)));
-                state++;
-                break;
-            case 1:
-                break;
-            default:
-                break;
         }
-        return null;
+
+        return new SendMessage().setChatId(message.getChatId()).setText(botmsg);
     }
 }
